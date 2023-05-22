418ec5e911a8be5e47822cf7329b52e8df7a15de
mnyef <vladgav004@gmail.com>
2023-05-22 16:43:01 +0300

    User Model

---
+++ models/User.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+import validator from 'validator';
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide name'],
+    minlength: 3,
+    maxlength: 20,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide email'],
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: 'Please provide a valid email',
+    },
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide password'],
+    minlength: 6,
+  },
+  lastName: {
+    type: String,
+    maxlength: 20,
+    trim: true,
+    default: 'LastName',
+  },
+  location: {
+    type: String,
+    maxlength: 20,
+    trim: true,
+    default: 'My city',
+  },
+});
+
+export default mongoose.model('User', UserSchema);
