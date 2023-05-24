9cf01ce4a6136d41e051cc83bca7c2826b54fac5
mnyef <vladgav004@gmail.com>
2023-05-24 22:12:42 +0300

    Dashboard Pages and Protected Route

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,33 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Dashboard, Error, Landing, Register } from './pages';
+import { Error, Landing, Register, ProtectedRoute } from './pages';
+import {
+  AddJob,
+  AllJobs,
+  Profile,
+  Stats,
+  SharedLayout,
+} from './pages/dashboard';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Dashboard />} />
+        {/* Dashboard */}
+        <Route
+          path='/'
+          element={
+            <ProtectedRoute>
+              <SharedLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<Stats />} />
+          <Route path='all-jobs' element={<AllJobs />}></Route>
+          <Route path='add-job' element={<AddJob />}></Route>
+          <Route path='profile' element={<Profile />}></Route>
+        </Route>
+
+        {/* Landing */}
         <Route path='/register' element={<Register />} />
         <Route path='/landing' element={<Landing />} />
         <Route path='*' element={<Error />} />
